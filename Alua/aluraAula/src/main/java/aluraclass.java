20225f1d6448675117bf7d588f45b925ef1fdc7f
pedrojasminDev <pluiz.jasmin@gmail.com>
2026-02-24 19:39:21 -0300

    Add files via upload

---
+++ Alua/aluraAula/src/main/java/aluraclass.java
@@ -0,0 +1,37 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class aluraclass {
+
+    // Exercicio 1
+    /*double nota1 = 8.5;
+    double nota2 = 7.0;
+
+    double media = (nota1 + nota2) / 2;
+
+    public void exibirMedia() {
+        System.out.println("A média é: " + media);
+    }*/
+
+    // Exercicio 2
+    /*double nota1 = 8.5;
+    int nota2 = 7;
+
+    public void exibirMedia() {
+        int media = (int) (nota1 + nota2) / 2;
+        System.out.println("A média é: " + media);
+    }*/
+
+    // Exercicio 3 concatenar char com string
+    /*char letra = 'A';
+    String palavra = "Alura";
+
+    public void exibirConcatenacao() {
+        String resultado = letra + palavra;
+        System.out.println("Resultado da concatenação: " + resultado);
+    }*/
+
+    
+
+
+
+}
